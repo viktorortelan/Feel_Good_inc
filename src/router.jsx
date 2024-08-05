db66d5958e486d1cef2e24be8b0d5707b191ead0
viktorortelan <jvortelan@gmail.com>
2024-08-05 19:57:35 -0300

    routes

---
+++ src/router.jsx
@@ -0,0 +1,11 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+export default function Site() {
+    return(
+        <BrowserRouter>
+      <Routes>
+        <Route />
+      </Routes>
+    </BrowserRouter>
+  );
+}
