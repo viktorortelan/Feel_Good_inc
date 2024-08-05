6f872f3dab76b8e23f486de4073f78d0dd956775
viktorortelan <jvortelan@gmail.com>
2024-08-05 20:27:43 -0300

    pastas do Home

---
+++ src/pages/home/index.jsx
@@ -0,0 +1,14 @@
+
+import './index.scss';
+import { Link } from 'react-router-dom';
+
+export default function Home() {
+
+    return(
+        <div className="pagina_home">
+            <div className="cabecalho">
+                
+            </div>
+        </div>
+    )
+}
