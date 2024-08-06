99efb36d2a77414a9b77a2025e3b7414e46ff5d5
viktorortelan <jvortelan@gmail.com>
2024-08-05 23:08:35 -0300

    card dos predios

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -17,6 +17,31 @@ export default function Home() {
                 <Link to="/">Contatos</Link>
                 <Link to="/">Sobre nós</Link>
             </div>
+            <div className="banners">
+                <img src="/assets/images/banner 1.png" alt="banner1" />
+            </div>
+
+            <div className="secao1">
+                <div className="card">
+                    <img src="/assets/images/predios.jpg" alt="predio" />
+                    <h1 id='titulo_card'>Leopodino</h1>
+                    <div className="info1">
+                        <h1 id='status'>Pronto</h1>
+                        <p id='tipo'>Residencial</p>
+                    </div>
+
+                    <div className="infoLoca">
+                        <img src="/assets/images/loca.png" alt="localização" /> 
+                        <h1 id='localiza'> Moema</h1>
+                    </div>
+                    
+                    <p id="cinza">4 comodos, 240M</p>
+                    <div className="info2">
+                        <h1><img src="/assets/images/carrin.png" alt="carrinho_vaga" /> 2 vagas</h1>
+                        <Link to="/"> Saiba mais</Link>
+                    </div>
+                </div>
+            </div>
         </div>
     )
 }
