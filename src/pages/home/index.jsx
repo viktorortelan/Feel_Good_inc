42d4b83360e398097eb154a14e2cc406c3aa6fd4
viktorortelan <jvortelan@gmail.com>
2024-08-05 23:38:41 -0300

    cards prontos

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -31,17 +31,62 @@ export default function Home() {
                     </div>
 
                     <div className="infoLoca">
-                        <img src="/assets/images/loca.png" alt="localização" /> 
+                        <img id='loca' src="/assets/images/loca.png" alt="localização" /> 
                         <h1 id='localiza'> Moema</h1>
                     </div>
-                    
+
                     <p id="cinza">4 comodos, 240M</p>
                     <div className="info2">
-                        <h1><img src="/assets/images/carrin.png" alt="carrinho_vaga" /> 2 vagas</h1>
-                        <Link to="/"> Saiba mais</Link>
+                    <img src="/assets/images/carrin.png" alt="carrinho_vaga" />
+                        <h1 id='vagas'> 2 vagas</h1>
+                        <Link to="/" id='saiba_mais'> Saiba mais</Link>
                     </div>
                 </div>
+
+                <div className="card2">
+                    <img src="/assets/images/predios.jpg" alt="predio" />
+                    <h1 id='titulo_card'>Mona lisa</h1>
+                    <div className="info1">
+                        <h1 id='status'>Pronto</h1>
+                        <p id='tipo'>Residencial</p>
+                    </div>
+
+                    <div className="infoLoca">
+                        <img id='loca' src="/assets/images/loca.png" alt="localização" /> 
+                        <h1 id='localiza'> Vila Olimpia</h1>
+                    </div>
+
+                    <p id="cinza">5 comodos, 280M</p>
+                    <div className="info2">
+                    <img src="/assets/images/carrin.png" alt="carrinho_vaga" />
+                        <h1 id='vagas'> 3 vagas</h1>
+                        <Link to="/" id='saiba_mais'> Saiba mais</Link>
+                    </div>
+                </div>
+                <div className="card3">
+                    <img src="/assets/images/predios.jpg" alt="predio" />
+                    <h1 id='titulo_card'>Star</h1>
+                    <div className="info1">
+                        <h1 id='status'>Pronto</h1>
+                        <p id='tipo'>Residencial</p>
+                    </div>
+
+                    <div className="infoLoca">
+                        <img id='loca' src="/assets/images/loca.png" alt="localização" /> 
+                        <h1 id='localiza'> Morumbi</h1>
+                    </div>
+
+                    <p id="cinza">6 comodos, 300M</p>
+                    <div className="info2">
+                    <img src="/assets/images/carrin.png" alt="carrinho_vaga" />
+                        <h1 id='vagas'> 2 vagas</h1>
+                        <Link to="/" id='saiba_mais'> Saiba mais</Link>
+                    </div>
+                </div>
+
+                
             </div>
+           
         </div>
     )
 }
