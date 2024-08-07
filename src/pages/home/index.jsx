0e3698df0ea81d92b70f6c6a7c838cc0139d39dc
viktorortelan <jvortelan@gmail.com>
2024-08-07 15:59:41 -0300

    pagina principal completa :)

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -97,9 +97,223 @@ export default function Home() {
 
             </div>
                   
-            <h1 id='um'>Promoção</h1>
-                    <h1 id='dois'>Promoção</h1>
-                    <h1 id='tres'>Promoção</h1>
+            <div className="missoes">
+                <div className="missao1">
+                    <img src="/assets/images/casinha.png" alt="casinha" />
+                    <p>Milhares de opções para encontrar</p>
+                    <p id="red">o imóvel que combina com você </p>
+                </div>
+
+                <div className="risco"></div>
+
+                <div className="missao2">
+                    <img src="/assets/images/joinha.png" alt="joia" />
+                    <p>Uma rede nacional com mais de</p>
+                    <p id="red">9 mil consultores locais especializados </p>
+                </div>
+
+                <div className="risco2"></div>
+
+                <div className="missao3">
+                    <img src="/assets/images/coracao.png" alt="joia" />
+                    <p>Inteligência de mercado para</p>
+                    <p id="red">te ajudar a fazer o melhor negócio </p>
+                </div>
+            </div>
+
+            <div className="tituloPredios">
+                <h1>Inspire-se com a nossa seleção</h1>
+                <p>Confira as oportunidades que separamos para você</p>
+            </div>
+
+            <div className="secao2">
+            <div className="card">
+                    <img src="/assets/images/predios.jpg" alt="predios" />
+                    <h1 id='titulo'>Folianopolis</h1>
+
+                    <div className="info1">
+                        <h1>Pronto</h1>
+                        <p>Residencial</p>
+                    </div>
+
+                    <div className="localizacao">
+                        <img src="/assets/images/loca.png" alt="localizão" />
+                        <p>Moema</p>
+                    </div>
+
+                    <h1 id='cinza'>5 comodos, 240M</h1>
+
+                    <div className="infoFinais">
+                        <img src="/assets/images/carrin.png" alt="carro" />
+                        <p>2 vagas </p>
+                        <Link to="/">Saiba mais</Link>
+                    </div>
+                  </div>
+
+                  <div className="card1">
+                    <img src="/assets/images/predios.jpg" alt="predios" />
+                    <h1 id='titulo'>Star</h1>
+
+                    <div className="info1">
+                        <h1>Pronto</h1>
+                        <p>Residencial</p>
+                    </div>
+
+                    <div className="localizacao">
+                        <img src="/assets/images/loca.png" alt="localizão" />
+                        <p>Vila olimpia</p>
+                    </div>
+
+                    <h1 id='cinza'>3 comodos, 180M</h1>
+
+                    <div className="infoFinais">
+                        <img src="/assets/images/carrin.png" alt="carro" />
+                        <p>1 vagas </p>
+                        <Link to="/">Saiba mais</Link>
+                    </div>
+                  </div>
+
+                  <div className="card3">
+                    <img src="/assets/images/predios.jpg" alt="predios" />
+                    <h1 id='titulo'>Cracolandia</h1>
+
+                    <div className="info1">
+                        <h1>Pronto</h1>
+                        <p>Residencial</p>
+                    </div>
+
+                    <div className="localizacao">
+                        <img src="/assets/images/loca.png" alt="localizão" />
+                        <p>Centro</p>
+                    </div>
+
+                    <h1 id='cinza'>0 comodos, 0M</h1>
+
+                    <div className="infoFinais">
+                        <img src="/assets/images/carrin.png" alt="carro" />
+                        <p>0 vagas </p>
+                        <Link to="/">Saiba mais</Link>
+                    </div>
+                  </div>
+
+                  <div className="card4">
+                    <img src="/assets/images/predios.jpg" alt="predios" />
+                    <h1 id='titulo'>Cracolandia</h1>
+
+                    <div className="info1">
+                        <h1>Pronto</h1>
+                        <p>Residencial</p>
+                    </div>
+
+                    <div className="localizacao">
+                        <img src="/assets/images/loca.png" alt="localizão" />
+                        <p>Centro</p>
+                    </div>
+
+                    <h1 id='cinza'>0 comodos, 0M</h1>
+
+                    <div className="infoFinais">
+                        <img src="/assets/images/carrin.png" alt="carro" />
+                        <p>0 vagas </p>
+                        <Link to="/">Saiba mais</Link>
+                    </div>
+                  </div>
+            </div>
+
+            <div className="secao3">
+            <div className="card">
+                    <img src="/assets/images/predios.jpg" alt="predios" />
+                    <h1 id='titulo'>Folianopolis</h1>
+
+                    <div className="info1">
+                        <h1>Pronto</h1>
+                        <p>Residencial</p>
+                    </div>
+
+                    <div className="localizacao">
+                        <img src="/assets/images/loca.png" alt="localizão" />
+                        <p>Moema</p>
+                    </div>
+
+                    <h1 id='cinza'>5 comodos, 240M</h1>
+
+                    <div className="infoFinais">
+                        <img src="/assets/images/carrin.png" alt="carro" />
+                        <p>2 vagas </p>
+                        <Link to="/">Saiba mais</Link>
+                    </div>
+                  </div>
+
+                  <div className="card1">
+                    <img src="/assets/images/predios.jpg" alt="predios" />
+                    <h1 id='titulo'>Star</h1>
+
+                    <div className="info1">
+                        <h1>Pronto</h1>
+                        <p>Residencial</p>
+                    </div>
+
+                    <div className="localizacao">
+                        <img src="/assets/images/loca.png" alt="localizão" />
+                        <p>Vila olimpia</p>
+                    </div>
+
+                    <h1 id='cinza'>3 comodos, 180M</h1>
+
+                    <div className="infoFinais">
+                        <img src="/assets/images/carrin.png" alt="carro" />
+                        <p>1 vagas </p>
+                        <Link to="/">Saiba mais</Link>
+                    </div>
+                  </div>
+
+                  <div className="card3">
+                    <img src="/assets/images/predios.jpg" alt="predios" />
+                    <h1 id='titulo'>Cracolandia</h1>
+
+                    <div className="info1">
+                        <h1>Pronto</h1>
+                        <p>Residencial</p>
+                    </div>
+
+                    <div className="localizacao">
+                        <img src="/assets/images/loca.png" alt="localizão" />
+                        <p>Centro</p>
+                    </div>
+
+                    <h1 id='cinza'>0 comodos, 0M</h1>
+
+                    <div className="infoFinais">
+                        <img src="/assets/images/carrin.png" alt="carro" />
+                        <p>0 vagas </p>
+                        <Link to="/">Saiba mais</Link>
+                    </div>
+                  </div>
+
+                  <div className="card4">
+                    <img src="/assets/images/predios.jpg" alt="predios" />
+                    <h1 id='titulo'>Cracolandia</h1>
+
+                    <div className="info1">
+                        <h1>Pronto</h1>
+                        <p>Residencial</p>
+                    </div>
+
+                    <div className="localizacao">
+                        <img src="/assets/images/loca.png" alt="localizão" />
+                        <p>Centro</p>
+                    </div>
+
+                    <h1 id='cinza'>0 comodos, 0M</h1>
+
+                    <div className="infoFinais">
+                        <img src="/assets/images/carrin.png" alt="carro" />
+                        <p>0 vagas </p>
+                        <Link to="/">Saiba mais</Link>
+                    </div>
+                  </div>
+            </div>
+            <Link to="/"><img src="/assets/images/loganfeelgood.png" alt="balão de suporte" id="suporte" /></Link>
            
         </div>
     )
